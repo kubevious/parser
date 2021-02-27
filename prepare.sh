945612f10e47b8121a999e25bcfb1a4d76b07976
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-02-26 18:33:07 -0800

    dependency changes.

--- prepare.sh
+++ prepare.sh
@@ -3,12 +3,7 @@ MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE
 MY_DIR="$(dirname $MY_PATH)"
 cd $MY_DIR
 
-# cd mock
-# rm -rf node_modules/
-# npm install
-# cd ..
-
 rm -rf node_modules/
+
 npm install
-npm install --only=dev
 npm update k8s-super-client the-lodash the-logger the-promise @kubevious/helpers  @kubevious/worldvious-client @kubevious/helper-backend @kubevious/helper-logic-processor
