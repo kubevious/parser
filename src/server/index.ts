9be2c4de743f59fff8a08524eae116b9d31aaa59
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-04-01 13:46:43 -0700

    server interface

--- src/server/index.ts
+++ src/server/index.ts
@@ -18,7 +18,10 @@ export class WebServer
         this.logger = context.logger.sublogger('WebServer');
         this.helpers = {
         };
-        this.server = new Server(this.logger, context, 4002, Path.join(__dirname, '..', 'routers'), this.helpers );
+        this.server = new Server(this.logger, context, this.helpers, {
+            port: 4002,
+            routersDir: Path.join(__dirname, '..', 'routers')
+        });
         this.server.initializer((app) => {
             app.set('trust proxy', true);
         })
