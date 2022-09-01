18a314dd4f10a487fb1a7883b8827d4970e5756b
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-09-01 15:26:05 -0700

    updated runner scripts.

---
+++ run-dev-k8s.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+MY_DIR="$(dirname $MY_PATH)"
+cd $MY_DIR
+
+source configuration.sh
+
+export LOG_TO_FILE=true
+export NODE_ENV=development
+export KUBEVIOUS_COLLECTOR=http://localhost:4003/api/v1/collect
+
+./build.sh
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "Build failed"
+  exit 1;
+fi
+
+source ../dependencies.git/k8s/configuration.sh
+export KUBECONFIG=${K8S_CONFIG_PATH}
+
+node dist/mock/index-remote
