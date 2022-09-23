513ca59b88843dea846220b854aadc1159f508ba
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-09-23 15:21:57 -0700

    added UBI Image build

--- configuration.sh
+++ configuration.sh
@@ -2,5 +2,6 @@ export SERVER_PORT=4004
 export CONTAINER_NAME=kubevious-parser
 export NETWORK_NAME=kubevious
 export IMAGE_NAME=kubevious-parser
+export IMAGE_NAME_UBI=${IMAGE_NAME}-ubi
 export MOCK_IMAGE_NAME=${IMAGE_NAME}-mock
 
---
+++ run-docker-ubi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+MY_DIR="$(dirname $MY_PATH)"
+cd $MY_DIR
+
+source configuration.sh
+
+docker run \
+    -it \
+    --rm \
+    --name "${CONTAINER_NAME}" \
+    -h ${CONTAINER_NAME} \
+    -p ${SERVER_PORT}:${SERVER_PORT} \
+    --network ${NETWORK_NAME} \
+    -e SERVER_PORT=${SERVER_PORT} \
+    -e KUBEVIOUS_COLLECTOR=http://kubevious-collector:4003/api/v1/collect \
+    ${IMAGE_NAME_UBI}
